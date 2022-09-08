aedf403d69f5539aec471a7cd1cc1a44fd230809
lowtze <>
2022-09-08 09:42:58 -0500

    cleaned up colors

--- violentmonkey-hn-dark.js
+++ violentmonkey-hn-dark.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://news.ycombinator.com/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      -
 // @description styling it a bit
 // ==/UserScript==
@@ -21,10 +21,10 @@ function addGlobalStyle(css) {
 addGlobalStyle(`
 * { line-height: 1.6 !important; }
 #hnmain, body { background-color: #222222 !important; }
-.rank, .score, .clicky { color: #FF6600 !important; }
+.rank, .hnuser { color: #FF6600 !important; }
 .titlelink, .yclinks a, center a, .comment .commtext, .subtext { color: white !important; }
 .votearrow { display: none !important; }
-.sitestr, .hnuser, .subtext > a[href^="item"] { color: #ff944c !important; }
+.sitestr, .score, .subtext > a[href^="item"], span.navs > a:last-child { color: #ff944c !important; }
 .itemlist a:visited { color: #BF6F39 !important; }
 div.comment { max-width: 60em !important; }
 `);
